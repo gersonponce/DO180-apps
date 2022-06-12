c5097a982ed8885fe86a70f9c068d89eeb9a43cb
Student User <student@workstation.lab.example.com>
2022-06-12 16:41:33 -0400

    actualizacion

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,5 @@
 <?php
-print "Hola mundo, la version de php es  " . PHP_VERSION . "\n";
+print "Hola mundo, la version de php es  " . PHP_VERSION . "\n<br>";
+echo "<h1>Esta es una actualizacion de la aplicacion. Sistemas distribuidos<h1>"
+
 ?>
